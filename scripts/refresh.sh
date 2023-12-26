d0c4fcf187a46a4eb27941f2a2bd4b266f35d18f
vishal <vishal340@github.com>
2023-12-26 22:35:11 +0530

    some mistakes fixed in script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -3,7 +3,6 @@
 #this script lets you change the refresh rate of monitor of your choice in multiple monitor setup.
 
 screens=$(($(xrandr | grep -o 'Screen ' | wc -l)))
-echo $screens
 if [ $screens -eq 1 ]; then
 	resolution=$(xrandr | awk '/.*\*.*/{print $1}')
 	response=$(($(xrandr | awk '/.*\*.*/{
@@ -14,14 +13,14 @@ else
 	choice=0
 	mon=0
 	for j in $(seq $screens); do
-		choice=$(($(echo $j | dmenu -l 1 -m $((j - 1)) -p "Enter:")))
+		choice=$(($(echo $j | dmenu -l 1 -m $((j - 1)) -p "Press Enter:")))
 		if [ $choice -gt 0 ]; then
 			mon=$((j))
 			break
 		fi
 	done
-	resolution=$(xrandr | awk -v var="$mon" '/.*\*.*/{count++; if(count==var)print $1}')
-	response=$(($(xrandr | awk -v var="$mon" '/.*\*.*/{count++;
+	resolution=$(xrandr | awk -v var="$mon" -v count=0 '/.*\*.*/{count++; if(count==var)print $1}')
+	response=$(($(xrandr | awk -v var="$mon" -v count=0 '/.*\*.*/{count++;
 	if(count==var)
 for(i=2;i<=NF;i++)if($i!="+")print $i
 }' | sed -E 's/(.*)\..*/\1/' | tr " " "\n" | dmenu -m $((mon)) -l 20 -i -p "choose refresh rate:")))
